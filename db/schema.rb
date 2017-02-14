b7ee561d0b6a8f2691a4a6111565625add514ac8
Patrick Conde <patrick.conde.costas@gmail.com>
2017-02-14 11:22:14 +0100

    Added release date column to games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214101031) do
+ActiveRecord::Schema.define(version: 20170214101357) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20170214101031) do
     t.string   "format"
     t.integer  "msrp"
     t.boolean  "multiplayer"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "release_date"
   end
 
 end
