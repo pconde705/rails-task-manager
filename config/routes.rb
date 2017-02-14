7155784bdc98de6948e43e44624605e9ca4d0211
Patrick Conde <patrick.conde.costas@gmail.com>
2017-02-14 16:02:46 +0100

    Really pay attention to all the connections

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # get 'games', to: "games#index"
+
+  # get 'games/:id', to: "games#show", as: "game"
+
+  # get 'games/new', to: "games#new"
+
+  # post 'games', to: "games#create"
+
+  # get 'games/:id/edit', to: "games#edit"
+
+  # patch 'games/:id', to: "games#update"
+
+  # delete 'games/:id', to: "games#destroy"
+
+  resources :games # writing this do all the above, and create prefixes automatically
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
